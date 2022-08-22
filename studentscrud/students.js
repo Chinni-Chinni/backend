c3efbcfeabf8640e8539dbe3142863523f8619dd
Chinni <enjarappachinni2001@gmail.com>
2022-08-22 11:50:19 +0530

    studentscrud

---
+++ studentscrud/students.js
@@ -0,0 +1,11 @@
+db.students_marks.find({gender : "Female"}).count()
+db.students_marks.find({gender : "Male",$and : [{maths : {$gt:85}},{science : {$gt : 85}},{english:{$gt:85}}]}).count()
+// other option
+db.students_marks.find({gender : "Male",maths : {$gt:85}, science : {$gt : 85},english:{$gt:85}}).count()
+db.students_marks.find({maths : {$gt : 50 , $lt : 75},english : {$gt : 50, $lt : 75}}).count()
+db.students_marks.find({class : {$in :["I","II","III","IV","V"]},maths : {$gt : 50},science : {$gt : 50},english : {$gt : 50}}).count()
+db.students_marks.find({class : "X",section : "A",maths : {$lt : 25},science : {$lt : 25},english : {$lt : 25}}).count()
+db.students_marks.find({class: "V"}).sort({maths : -1}).limit(3);
+db.students_marks.find({class : "I"}).sort({science : 1}).limit(5);
+db.students_marks.find({section : "A",maths : {$lt : 50},science : {$lt : 50},english : {$lt : 50}}).count()
+db.students_marks.find({section : "C",maths : {$gt :75},science : {$gt :75},english : {$gt : 75}})
